ec3ac8933144eec68198e3329a18968531ec5ba5
Sandeep Poojari <sandeepvp110@gmail.com>
2026-01-21 23:42:12 +0100

    Blink user LED on B-L4S5I-IOT01A using led0 alias

--- app/src/main.c
+++ app/src/main.c
@@ -1,12 +1,28 @@
 #include <zephyr/kernel.h>
-#include <zephyr/sys/printk.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+
+#define LED_NODE DT_ALIAS(led0)
+
+#if !DT_NODE_HAS_STATUS(LED_NODE, okay)
+#error "led0 alias is not defined in the device tree"
+#endif
+
+static const struct gpio_dt_spec led =
+    GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
 void main(void)
 {
-    printk("Zephyr is alive\n");
+    if (!device_is_ready(led.port)) {
+        while (1) {
+            k_sleep(K_FOREVER);
+        }
+    }
+
+    gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
 
     while (1) {
-        printk("tick\n");
+        gpio_pin_toggle_dt(&led);
         k_sleep(K_SECONDS(1));
     }
 }
